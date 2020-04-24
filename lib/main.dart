3d7cd6268dfbd7980198503184010456685e0ad9
Matheus Carneiro <matheus10carneiro@gmail.com>
2020-04-24 12:25:47 -0300

    REFATORACAO DE CODDIGO

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,66 @@
+import 'package:covid19/src/pages/about.dart';
+import 'package:covid19/src/pages/countries.dart';
+import 'package:covid19/src/pages/home.dart';
+import 'package:covid19/src/pages/states.dart';
 import 'package:flutter/material.dart';
-import 'package:covid19/src/pages/init.dart';
+import 'package:flutter/services.dart';
+import 'package:page_indicator/page_indicator.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Init(),
+    home: MainApp(),
     debugShowCheckedModeBanner: false,
   ));
 }
+
+class MainApp extends StatefulWidget {
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+
+  PageController _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    SystemChrome.setEnabledSystemUIOverlays([]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageIndicatorContainer(
+        child: PageView(
+          children: <Widget>[
+            Home(),
+            States(),
+            Countries(),
+            About(),
+          ],
+          controller: _controller,
+        ),
+        align: IndicatorAlign.bottom,
+        length: 4,
+        indicatorSpace: 20.0,
+        padding: const EdgeInsets.all(10),
+        indicatorColor: Color(0xB3063d61),
+        indicatorSelectorColor: Colors.white,
+        shape: IndicatorShape.circle(size: 12));
+  }
+}
+
+
